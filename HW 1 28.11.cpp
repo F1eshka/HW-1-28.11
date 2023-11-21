07c7d11cb4b6668e0e1aaa542cb5facb3483d40f
F1eshka <144143888+F1eshka@users.noreply.github.com>
2023-11-21 14:48:41 +0300

    Add files via upload

---
+++ HW 1 28.11.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int length;
+	length = 50;
+	char symbol = ' ';
+	int spaces = 0;
+	int wordcount = 0;
+	int nospaces = 0;
+	int sr = 0;
+	char* name = new char[length] {};
+	cout << name << "\n";
+	cout << "Введите текст: ";
+	gets_s(name, length);
+	for (int i = 0; i < strlen(name); i++)
+	{
+		cout << name[i] << "";
+		if (name[i] == symbol)
+		{
+			spaces++;
+		}
+	}
+	cout << "\n";
+	cout << "Пробелов: " << spaces << "\n";
+	wordcount = spaces + 1;
+	cout << "Количество слов: " << wordcount << "\n";
+	nospaces = strlen(name) - spaces;
+	cout << "Не пробельные символы: " << nospaces << "\n";
+	sr = nospaces / wordcount;
+	cout << "Средняя длина: " << sr << "\n";
+
+	delete[] name;
+}
